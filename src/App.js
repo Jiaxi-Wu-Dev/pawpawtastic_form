fa5f68f960f5d153a1e65e1a4110b5c05b7c27fc
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-27 14:39:13 -0400

    set up react app, created left component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Left from './components/Left'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="leftSide">
+        <Left />
+      </div>
+      <div className='rightSide'></div>
     </div>
   );
 }
---
+++ src/components/Left.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Left extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <div className="heading">
+                    <img src="https://pawsofcoronado.org/wp-content/uploads/2016/01/cropped-favicon.png"></img>
+                    <h1>Pawtastic</h1>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Left;
