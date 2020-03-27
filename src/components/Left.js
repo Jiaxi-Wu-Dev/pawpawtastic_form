5bc5a2d0f604f4306f9f5a2a994772b9f7f92949
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-27 14:53:46 -0400

    added Right component

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Left extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <div className="heading">
-                    <img src="https://pawsofcoronado.org/wp-content/uploads/2016/01/cropped-favicon.png"></img>
-                    <h1>Pawtastic</h1>
-                </div>
-            </div>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <div className="containerDiv">
+        <div className="heading">
+          <img
+            src="https://pawsofcoronado.org/wp-content/uploads/2016/01/cropped-favicon.png"
+            width="70px"
+          ></img>
+          <h1>Pawtastic</h1>
+        </div>
+        <div className="leftList">
+          <form>
+            <ul>
+              <li>Human Profile</li>
+              <li>Pet Basics</li>
+              <li>Pet Details</li>
+              <li>Confirm</li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Left;
